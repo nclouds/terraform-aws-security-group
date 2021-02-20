30d6f2d6d054070d30706d4593d7bd9973f56f1e
Anup Gupta <anup@nclouds.com>
2021-02-20 13:40:54 +1100

    docs: Add working examples and improve documentation

---
+++ examples/advanced/main.tf
@@ -0,0 +1,59 @@
+# Create a VPC
+module "vpc" {
+  count        = var.create_vpc ? 1 : 0
+  source       = "git@github.com:nclouds/terraform-aws-vpc.git?ref=v0.1.0"
+  multi_nat_gw = false
+  vpc_settings = {
+    application_subnets = ["10.10.16.0/22", "10.10.20.0/22"]
+    public_subnets      = ["10.10.0.0/22", "10.10.4.0/22"]
+    dns_hostnames       = true
+    data_subnets        = []
+    dns_support         = true
+    tenancy             = "default"
+    cidr                = "10.10.0.0/16"
+  }
+  identifier = "${var.identifier}_vpc"
+  region     = "us-east-1"
+  tags = {
+    Owner = "sysops"
+    env   = "dev"
+  }
+}
+
+# Create a Security Group
+module "security_group" {
+  source     = "../.."
+  identifier = "${var.identifier}-sg"
+  vpc_id     = var.create_vpc ? module.vpc[0].output.vpc.id : "vpc-000fe2b5ddba6bb64"
+  ingress_rule_list = [
+    {
+      cidr_blocks = ["0.0.0.0/0"],
+      description = "HTTPS inbound",
+      from_port   = 443,
+      protocol    = "tcp",
+      to_port     = 443
+    },
+    {
+      cidr_blocks = ["0.0.0.0/0"],
+      description = "HTTP inbound",
+      from_port   = 80,
+      protocol    = "tcp",
+      to_port     = 80
+  }]
+  egress_rule_list = [{
+    cidr_blocks = ["0.0.0.0/0"],
+    description = "HTTPS outbound",
+    from_port   = 443,
+    protocol    = "tcp",
+    to_port     = 443
+    },
+    {
+      cidr_blocks = ["0.0.0.0/0"],
+      description = "HTTP outbound",
+      from_port   = 80,
+      protocol    = "tcp",
+      to_port     = 80
+    }
+  ]
+  tags = var.tags
+}
---
+++ examples/advanced/variables.tf
@@ -0,0 +1,21 @@
+variable "create_vpc" {
+  description = "Create a new VPC"
+  default     = false
+  type        = bool
+}
+
+variable "identifier" {
+  description = "Name of the resources"
+  default     = "example"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the resource"
+  default = {
+    Owner       = "sysops"
+    env         = "dev"
+    Cost_Center = "XYZ"
+  }
+  type = map
+}
