1784137d3ea69f66fa42fe1aecc4bc71e574453e
Anup Gupta <anup.cloud.devops@gmail.com>
2022-06-12 10:56:16 +1000

    Enable OPA Checks and Update Module References to Github

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -1,9 +1,63 @@
+locals {
+  policies = {
+    logs = {
+      actions = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      resources = [
+        "*",
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "document" {
+  statement {
+    actions   = local.policies["logs"]["actions"]
+    resources = local.policies["logs"]["resources"]
+  }
+}
+
+module "iam_policy" {
+  source = "git@github.com:nclouds/terraform-aws-iam-policy.git?ref=v0.1.4"
+
+  rendered_policy = data.aws_iam_policy_document.document.json
+  description     = "IAM Policy for VPC Flow Logs Cloudwatch"
+  identifier      = "cw-flow-logs-access"
+  tags            = var.tags
+}
+
+module "flow_logs_role" {
+  source = "git@github.com:nclouds/terraform-aws-iam-role.git?ref=v0.2.2"
+
+  iam_policies_to_attach = [
+    module.iam_policy.output.policy.arn
+  ]
+
+  aws_service_principal = "vpc-flow-logs.amazonaws.com"
+  description           = "Example IAM Role"
+  identifier            = "${var.identifier}-flow-logs"
+  tags                  = var.tags
+}
+
 # Create a VPC
 module "vpc" {
   count        = var.create_vpc ? 1 : 0
-  source       = "app.terraform.io/ncodelibrary/vpc/aws"
-  version      = "0.3.0"
+  source       = "git@github.com:nclouds/terraform-aws-vpc.git?ref=v0.3.0"
   multi_nat_gw = false
+  flow_log_settings = {
+    max_aggregation_interval = 600
+    flow_log_destination_arn = null
+    logs_retention_in_days   = 30
+    log_destination_type     = "cloud-watch-logs"
+    enable_flow_log          = true
+    iam_role_arn             = module.flow_logs_role.output.role.arn
+    traffic_type             = "ALL"
+  }
   vpc_settings = {
     application_subnets = ["10.10.16.0/22", "10.10.20.0/22"]
     public_subnets      = ["10.10.0.0/22", "10.10.4.0/22"]
@@ -13,7 +67,7 @@ module "vpc" {
     tenancy             = "default"
     cidr                = "10.10.0.0/16"
   }
-  identifier = var.identifier
+  identifier = "${var.identifier}_vpc"
   region     = "us-east-1"
   tags       = var.tags
 }
